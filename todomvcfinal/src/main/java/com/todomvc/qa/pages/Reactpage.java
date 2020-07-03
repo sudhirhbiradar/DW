c52eb7220a37cd0b8583f29a95be776cf6e49c59
sudhirhbiradar <sudhirhbiradar@gmail.com>
2020-07-03 16:17:14 +0530

    it is done

---
+++ todomvcfinal/src/main/java/com/todomvc/qa/pages/Reactpage.java
@@ -0,0 +1,35 @@
+package com.todomvc.qa.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.todomvc.qa.base.Testbase;
+
+public class Reactpage extends Testbase {
+
+		@FindBy(xpath ="//a[@href='examples/react']")
+
+		private static WebElement reactbutton;
+		
+	public Reactpage() {
+		
+		PageFactory.initElements(driver, this);
+	}
+
+	public static Reactpage clickreactbutton1() {
+		
+		
+		reactbutton.click();
+		return new Reactpage();
+		
+		
+	}
+
+	public void clickreactbutton() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+}
